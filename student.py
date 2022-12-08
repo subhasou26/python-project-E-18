a39b0f370c6dbe024958018b4ea63aef549db58f
subha <subhadipp26@gmail.com>
2022-12-08 15:57:08 +0530

    persentage commit

--- student.py
+++ student.py
@@ -1,23 +1,23 @@
 import csv
 import pandas as pd
-student_fields = ['student_id', 'name', 'Class_roll_no','Batch_name']
-student_subject=[student_fields[0],'bengali','english','math','physics','chemistry']
+student_fields = ['student_id', 'name', 'Class_roll_no', 'Batch_name']
+student_subject = [student_fields[0], 'bengali',
+                   'english', 'math', 'physics', 'chemistry']
 
 student_database = 'students.csv'
-student_subject_database='marks.csv'
+student_subject_database = 'marks.csv'
+
 
 def number_report():
-     global student_fields
-     global student_subject_database
-     global student_database
-     global student_subject
-     print("--- Creat_report_card ---")
-     student_id= input("Enter student_id no. to make report card: ")
-     creat_report_card(student_id)
-     total_number(student_id)
+    global student_fields
+    global student_subject_database
+    global student_database
+    global student_subject
+    print("--- Creat_report_card ---")
+    student_id = input("Enter student_id no. to make report card: ")
+    creat_report_card(student_id)
+    total_number(student_id)
 
-
-     
 
 def total_number(student_id):
     global student_fields
@@ -25,69 +25,75 @@ def total_number(student_id):
     global student_database
     global student_subject
     with open(student_subject_database, "r", encoding="utf-8") as f:
-        csv_data=csv.reader(f)
-        count=0
+        csv_data = csv.reader(f)
+        count = 0
         for row in csv_data:
+
             if len(row) > 0:
-                
-                    file=open(student_id+".txt","a")
-                    i=1
-                    
-                    for i in range(1,6,1):
-                        y=int(row[i])
-                        count=y+count
-        if((count/5)<=40):
+                if student_id == row[0]:
+
+                    file = open(student_id+".txt", "a")
+                    i = 1
+
+                    for i in range(1, 6, 1):
+                        y = int(row[i])
+                        count = y+count
+                    persentage = str(count/5)
+        file.write("Your over all persentage is "+persentage+"\n")
+        if ((count/5) <= 40):
             file.write("Sorry You are fail")
-        file.write("You are pass")            
- 
+        file.write("You are pass")
+
+
 def creat_report_card(student_id):
     global student_fields
     global student_subject_database
     global student_database
     global student_subject
     with open(student_subject_database, "r", encoding="utf-8") as f:
-        csv_data=csv.reader(f)
+        csv_data = csv.reader(f)
         for row in csv_data:
             if len(row) > 0:
-                if student_id==row[0]:
-                    file=open(student_id+".txt","a")
+                if student_id == row[0]:
+                    file = open(student_id+".txt", "a")
                     file.write("marks"+"     Gread"+" Persentage "+"\n")
-                    i=1
-                    for i in range(1,6,1):
-                        s="SUBHA"
-                        y=int(row[i])
-                        if(y>=90):
-                            
-                            file.write(row[i]+"          A      "+row[i]+"%"+"\n")
-                        elif(y>=80):
-                            file.write(row[i]+"          B      "+row[i]+"%"+"\n")
-                        elif(y>=70):
-                            file.write(row[i]+"          C      "+row[i]+"%"+"\n")
-                        elif(y>=60):
-                            file.write(row[i]+"          D      "+row[i]+"%"+"\n")
-                        elif(y>=50):
-                            file.write(row[i]+"          E      "+row[i]+"%"+"\n")
+                    i = 1
+                    for i in range(1, 6, 1):
+                        s = "SUBHA"
+                        y = int(row[i])
+                        if (y >= 90):
+
+                            file.write(
+                                row[i]+"          A      "+row[i]+"%"+"\n")
+                        elif (y >= 80):
+                            file.write(
+                                row[i]+"          B      "+row[i]+"%"+"\n")
+                        elif (y >= 70):
+                            file.write(
+                                row[i]+"          C      "+row[i]+"%"+"\n")
+                        elif (y >= 60):
+                            file.write(
+                                row[i]+"          D      "+row[i]+"%"+"\n")
+                        elif (y >= 50):
+                            file.write(
+                                row[i]+"          E      "+row[i]+"%"+"\n")
                         else:
-                            
-                            file.write(row[i]+"          F      "+row[i]+"%"+"\n")
-                            
-                    
-                    
-                    
+
+                            file.write(
+                                row[i]+"          F      "+row[i]+"%"+"\n")
+
                     break
-                    
-                    
-                   
-                   
+
         else:
             print("student_id No. not found in our database")
             input("Press any key to continue")
             # file=open("report_card.txt","w")
             # file.write(row)
 
-        #file=open("Report_card.txt","w")
-         #file.write()
-          
+        # file=open("Report_card.txt","w")
+         # file.write()
+
+
 ''' student_found = False
     updated_data = []
     with open(student_subject_database, "r", encoding="utf-8") as f:
@@ -103,7 +109,7 @@ def creat_report_card(student_id):
 
     if student_found is True:
         '''
-     
+
 
 def display_menu():
     print("---------------------------------------")
@@ -114,7 +120,7 @@ def display_menu():
     print("3. Delete Student")
     print("4. Add Number")
     print("5. Report Card")
-    
+
 
 def add_number():
     print("-------------------------")
@@ -125,13 +131,15 @@ def add_number():
     global student_subject
     global student_subject_database
 
-    student_data=[]
+    student_data = []
     for field in student_subject:
-        value=input("Enter "+ field +":")
+        value = input("Enter " + field + ":")
         student_data.append(value)
-    with open(student_subject_database,"a",encoding="utf-8") as f:
-        writer=csv.writer(f)
-        writer.writerows([student_data])    
+    with open(student_subject_database, "a", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerows([student_data])
+
+
 def delete_student_number():
     global student_fields
     global student_subject_database
@@ -160,13 +168,13 @@ def delete_student_number():
 
     input("Press any key to continue")
 
+
 def add_student():
     print("-------------------------")
     print("Add Student Information")
     print("-------------------------")
     global student_fields
     global student_database
-
 
     student_data = []
     for field in student_fields:
@@ -225,6 +233,7 @@ def add_student():
     input("Press any key to continue")
 '''
 
+
 def update_student():
     global student_fields
     global student_database
@@ -240,7 +249,7 @@ def update_student():
             if len(row) > 0:
                 if student_id == row[0]:
                     index_student = counter
-                    print("Student Found: at index ",index_student)
+                    print("Student Found: at index ", index_student)
                     student_data = []
                     for field in student_fields:
                         value = input("Enter " + field + ": ")
@@ -250,8 +259,6 @@ def update_student():
                     updated_data.append(row)
                 counter += 1
 
-
-    
     if index_student is not None:
         with open(student_database, "w", encoding="utf-8") as f:
             writer = csv.writer(f)
@@ -291,27 +298,27 @@ def delete_student():
 
     input("Press any key to continue")
 
+
 while True:
     display_menu()
 
     choice = input("Enter your choice: ")
     if choice == '1':
         add_student()
-   
+
     elif choice == '2':
         update_student()
     elif choice == '3':
         delete_student()
-       
-    elif choice=='4':
+
+    elif choice == '4':
         add_number()
-       
-    elif choice=='5':
-       number_report()
+
+    elif choice == '5':
+        number_report()
     else:
-        break    
+        break
 
 print("-------------------------------")
 print(" Thank you for using our system")
 print("-------------------------------")
-
